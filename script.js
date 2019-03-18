8e0e6d16d7044f31a58e96ca8c7ca4055f2970b3
Łukasz Miłkowski <lukasz@platoteam.com>
2019-03-18 19:41:23 +0100

    functions

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 
-window.onload = function() {
-   var a = document.getElementById("radd").addEventListener("click", pusta);   
+window.onload = function () {
+    var a = document.getElementById("radd").addEventListener("click", pusta);
 }
+
 var x = 1;
+var y = 1;
+
+
 function pusta() {
-    if (document.getElementById("txt").value == ''){}
-    else {cozrobic()};
+    if (document.getElementById("txt").value == '') { }
+    else { cozrobic() };
+}
+
+function addIcon(name, onClick) {
+    var icon = new Image(20, 20);
+    icon.src = "https://img.icons8.com/" + name + ".png";
+    icon.onclick = onClick;
+    icon.style.cursor = "pointer";
+    return icon;
 }
 
 function cozrobic() {
-    
+
     var box = document.createElement("div");
-        box.id = "list" + x;
-            var p = document.createElement("p")
-                 p.innerHTML = document.getElementById("txt").value;
-                 box.appendChild(p);
-                 document.getElementById("txt").value = "";
-            var pencil = new Image(20,20);
-                pencil.src = "https://img.icons8.com/dotty/80/000000/pencil-tip.png";
-                box.appendChild(pencil);
-                pencil.onclick = myEdit;
-                pencil.style.cursor = "pointer";
-            var bin = new Image(20,20);
-                bin.src = "https://img.icons8.com/dotty/80/000000/trash.png";
-                box.appendChild(bin);
-                bin.onclick = myDelete;
-                bin.style.cursor = "pointer";
+    box.id = "list" + x;
+    var p = document.createElement("p")
+    p.innerHTML = document.getElementById("txt").value;
+    box.appendChild(p);
+    document.getElementById("txt").value = "";
+
+    box.appendChild(addIcon("dotty/80/000000/pencil-tip", myEdit));
+    box.appendChild(addIcon("dotty/80/000000/trash", myDelete));
     var parent = document.getElementById("container");
     parent.appendChild(box);
-    x++;    
+    x++;
 }
-y = 1;  
+
 function myEdit(ev) {
-   var b = ev.target;
-   var div = b.parentElement;
-   var p = div.firstChild;
-   var input = document.createElement("input");
-        input.id ="input" + y;
-   div.replaceChild(input,  p);
-        var check = new Image(20,20);
-        check.src = "https://img.icons8.com/material/24/000000/checkmark.png";
-        div.replaceChild(check, b);
-        check.onclick = accept;
-        check.style.cursor = "pointer";
-        y++;
+    var b = ev.target;
+    var div = b.parentElement;
+    var p = div.firstChild;
+    var input = document.createElement("input");
+    input.id = "input" + y;
+    div.replaceChild(input, p);
+    div.replaceChild(addIcon("material/24/000000/checkmark", accept), b);
+    y++;
 }
 
 function myDelete(ev) {
     var a = ev.target;
     a.parentElement.remove();
 }
-function accept(ev){
+
+function accept(ev) {
     var check = ev.target;
     var div = check.parentElement;
     var input = div.firstChild;
     var p = document.createElement("p");
     p.innerHTML = input.value;
-    div.replaceChild(p,input);
-    var pencil = new Image(20,20);
-    pencil.src = "https://img.icons8.com/dotty/80/000000/pencil-tip.png";
-    div.replaceChild(pencil , check);
-    pencil.onclick = myEdit;  
+    div.replaceChild(p, input);
+    div.replaceChild(addIcon("dotty/80/000000/pencil-tip", myEdit), check);
 }
